ffdada5812d367fec07bcc59537e68b0244eb6af
HarpreetPannu <er.harpreetpannu@live.com>
2023-11-20 22:44:51 -0800

    Added ViewLocator class to return view based on ViewModel parameter. Also, added functionality for AddingItem page.

--- ToDoList/App.axaml.cs
+++ ToDoList/App.axaml.cs
@@ -17,22 +17,22 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var service = new ToDoListService();
+        //var service = new ToDoListService();
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
 
             desktop.MainWindow = new MainWindow
             {
-                //DataContext = new MainViewModel()
-                DataContext = new ToDoListViewModel(service.getItems())
+                DataContext = new MainViewModel()
+                //DataContext = new ToDoListViewModel(service.getItems())
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                //DataContext = new MainViewModel()
-                DataContext = new ToDoListViewModel(service.getItems())
+                DataContext = new MainViewModel()
+                //DataContext = new ToDoListViewModel(service.getItems())
             };
         }
 
--- ToDoList/ViewModels/MainViewModel.cs
+++ ToDoList/ViewModels/MainViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace ToDoList.ViewModels;
+﻿using ReactiveUI;
+using ToDoList.Model.Services;
+
+namespace ToDoList.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private ViewModelBase _contentViewModel;
+    public ToDoListViewModel ToDoList {  get;}
+    public MainViewModel()
+    {
+        var service = new ToDoListService();
+        ToDoList = new ToDoListViewModel(service.getItems());
+        _contentViewModel = ToDoList;
+    }
+
+    public ViewModelBase ContentViewModel
+    {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
+
+    public void AddItem()
+    {
+        ContentViewModel = new AddItemViewModel();
+    }
+
 }
