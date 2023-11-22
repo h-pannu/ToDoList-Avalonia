b408bafb10adca8640babfbbb369d10ba5ac0ee3
HarpreetPannu <er.harpreetpannu@live.com>
2023-11-22 00:10:18 -0800

    ReactiveCommand for Ok and Cancel button.

--- ToDoList/ViewModels/AddItemViewModel.cs
+++ ToDoList/ViewModels/AddItemViewModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.Model.DataModel;
 
 namespace ToDoList.ViewModels
 {
     public class AddItemViewModel :ViewModelBase
     {
-        public string Description { get; set; } = string.Empty;
+        private string _description;
+
+        public string Description
+        {
+            get => _description;
+            set => this.RaiseAndSetIfChanged(ref _description, value);
+        }
+
+        public ReactiveCommand<Unit, ToDoItem> OkCommand { get; }
+        public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+
+        public AddItemViewModel()
+        {
+            var isValidObservable = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrEmpty(x));
+
+            OkCommand = ReactiveCommand.Create(
+                () => new ToDoItem { Description = Description}, isValidObservable );
+
+            CancelCommand=ReactiveCommand.Create(() => {  });
+        }
     }
 }
